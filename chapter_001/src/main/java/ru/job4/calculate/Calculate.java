e0738f216f1db6a80b1805047ccc1f99d91af29b
Alexsandr Arsenkov <mailto:parsentev@yandex.ruarsenkov22@mail.ru>
2018-07-05 23:02:00 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4/calculate/Calculate.java
@@ -1,7 +1,14 @@
 package ru.job4j.calculate;
-
+/**
+*Calculate.
+*@author Alexander Arsenkov(arsenkov22@mail.ru)
+*/
 
 public class Calculate {
+/**
+*Main.
+*@param args-args.
+*/
     
     public static void main(String[] args) {
         System.out.println("Hello World");
